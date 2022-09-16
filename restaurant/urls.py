bd60664f22bdf8b64cc6caa3c57f1f046f031e3c
paul-local <nwalipaul353@gmail.com>
2022-09-16 23:37:12 +0100

    completed my index hero section!

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home/', home, name='restaurant-home')
+    path('home/', home, name='restaurant-home'),
+    path('menu/', menu, name='restaurant-menu'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'restaurant/index.html')
+
+def menu(request):
+  return render(request, 'restaurant/menu.html')
