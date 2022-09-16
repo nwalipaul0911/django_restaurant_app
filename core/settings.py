6394d04aefa444683c2193694a16a2998b82ecd4
paul-local <nwalipaul353@gmail.com>
2022-09-16 16:35:21 +0100

    first progress from my workspace git branch

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'restaurant',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +122,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILE_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = (BASE_DIR/'assets')
+
+MEDIA_URL= '/media/'
+MEDIA_ROOT = (BASE_DIR/'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Login settings
+
+
+
+# EMAIL SETTINGS
